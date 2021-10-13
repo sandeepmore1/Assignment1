b972ed096ece24ef76d9e1b0f5263362416f3df6
sandeepmore1 <92455220+sandeepmore1@users.noreply.github.com>
2021-10-13 21:10:57 +0530

    Add files via upload

---
+++ Python Assignments 1.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+n1=[]
+for i in range (1500,3200):
+    if (i%7==0) and (i%5!=0):
+        n1.append(str(i))
+        print(','.join(n1))
+        
+    
+
+
+# In[1]:
+
+
+firstname = input("Input your First Name : ")
+lastname = input("Input your Last Name : ")
+print ("Hello  " + lastname + " " + firstname)
+
+
+# In[11]:
+
+
+D = 12
+r = D/2
+V = 4/3*(3.142*r*r*r)
+print(V)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
